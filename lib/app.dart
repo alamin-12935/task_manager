2e0f2589a27048fd354d622c247f3c0aa105e6c8
larning <alaminkhan133742@gmail.com>
2026-05-05 18:33:37 +0600

    3nd commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/screens/splash_screen.dart';
+import 'package:task_manager/utils/app_color.dart';
 class taskmanagerapp extends StatelessWidget {
   const taskmanagerapp({super.key});
 
@@ -12,7 +13,30 @@ class taskmanagerapp extends StatelessWidget {
             fontSize: 28,
             fontWeight: FontWeight.w600
           )
-        )
+        ),
+
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none
+          ),
+        ),
+
+          filledButtonTheme: FilledButtonThemeData(
+          style:FilledButton.styleFrom(
+            backgroundColor: AppColor.Pcolor,
+           fixedSize: Size.fromWidth(double.maxFinite),
+            padding: EdgeInsets.symmetric(vertical: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8)
+            )
+          )
+      )
       ),
       title: 'Task manager',
       debugShowCheckedModeBanner: false,
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,17 +12,29 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(child: 
-          Column(
-            children: [
-              SizedBox(height: 150,),
-              Text('Get Started With'),
-              SizedBox(height: 25,),
-              TextFormField(),
-              SizedBox(height: 10,),
-              TextFormField(),
-              SizedBox(height: 10,),
-              FilledButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined))
-            ],
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 150,),
+                Text('Get Started With',style: Theme.of(context).textTheme.titleLarge),
+                SizedBox(height: 25,),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email'
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'password'
+                  ),
+                ),
+                SizedBox(height: 10,),
+                FilledButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined))
+              ],
+            ),
           )
       ),
     );
